920be599d19840d47b88fe73e8508f090ba96503
Damien Martin <dammarti@iccluster028.iccluster.epfl.ch>
2018-11-19 21:40:15 +0100

    remove garbage for console

--- src/run.sh
+++ src/run.sh
@@ -2,4 +2,4 @@
 export PYTHONIOENCODING=utf8
 export RUN_ON_CLUSTER=1
 spark-submit \
---master yarn --deploy-mode client --driver-memory 32G --num-executors 5 --executor-memory 32G --executor-cores 10 "$@" 2>&1 | grep -v " INFO "
+--master yarn --deploy-mode client --driver-memory 32G --num-executors 5 --executor-memory 32G --executor-cores 10 "$@" 2>&1 | awk '/ACCEPTED/ || !/INFO/'
