f1d8bbeb2adff58d521c500813ed9c96b0459b80
Damien Martin <damien.martin@epfl.ch>
2018-11-30 18:44:31 +0100

    use master mode on cluster

--- src/cluster/run.sh
+++ src/cluster/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
 export PYTHONIOENCODING=utf8
 export RUN_ON_CLUSTER=1
+
+zip dependencies config.py load_datasets.py
+
 spark-submit \
---master yarn --deploy-mode client --driver-memory 32G --num-executors 5 --executor-memory 32G --executor-cores 10 "$@" 2>&1 | awk '/ACCEPTED/ || !/INFO/'
+--master yarn --deploy-mode cluster --driver-memory 32G --num-executors 5 --executor-memory 32G --executor-cores 10 --py-files dependencies.zip "$@" 2>&1 | awk '/ACCEPTED/ || !/INFO/'
