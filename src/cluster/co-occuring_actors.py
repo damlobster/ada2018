7d24fd8b8ea06ec89cb07f5e7880ced3e5aa3dcf
Mathieu Yves Marie Ducroux <ducroux@iccluster028.iccluster.epfl.ch>
2018-12-13 21:32:32 +0100

    add cooc cluster

--- src/cluster/co-occuring_actors.py
+++ src/cluster/co-occuring_actors.py
@@ -14,26 +14,33 @@ from load_datasets import get_from_hadoop
 
 spark = SparkSession.builder.getOrCreate()
 
-def load_actor_occ(date):
+top_50_persons = ["donald trump","barack obama","los angeles","pacific ocean","rick scott","el nino","atlantic ocean","hillary clinton",
+"jerry brown","abu dhabi","greg abbott","las vegas","pope francis","superstorm sandy","john bel edwards","santa barbara","vladimir putin"
+,"seth borenstein","john kerry","george w bush","malcolm turnbull","khalid al-falih","indian ocean","tony abbott","asia pacific",
+"bernie sanders","justin trudeau","nikki haley","janet yellen","scott pruitt","pat mccrory","al gore","angela merkel","narendra modi",
+"bertrand piccard","henning gloystein","sylvester turner","alexander novak","dennis feltgen","ricardo rossello","david cameron",
+"francois hollande","rex tillerson","arctic ocean","sally jewell","muhammadu buhari","andrew cuomo","andre borschberg","elon musk", "ali al-naimi"]
 
-  persons_df = spark.read.parquet(config.OUTPUT_PATH+"/gkg_withtheme_nonexploded.parquet").select("GKGRECORDID", "V2DATE", "V1Persons")
+top_50_organizations = ["associated press","united states","national weather service","reuters","twitter","facebook","cnn",
+"national hurricane center","white house","nasdaq","united nations","european union","u s geological","emergency management agency",
+"bloomberg","organization of the petroleum exporting countries","shell","google","u s national hurricane center",
+"atmospheric administration","international energy agency","national oceanic","national guard","federal reserve","dow jones",
+"environmental protection agency","instagram","supreme court","bureau of meteorology","york mercantile exchange","new york times",
+"world forestry congress","chevron","new york mercantile exchange","u s energy information administration","world bank","xinhua",
+"royal dutch shell","youtube","goldman sachs","exchange commission","energy information administration","u s forest service",
+"california department of forestry","exxon","washington post","earthquake notification service","american petroleum institute",
+"zacks investment research","storm prediction center"]
 
-  persons_df.createOrReplaceTempView("persons")
+def load_actor_occ():
 
-  query_persons = """
-  SELECT *
-  FROM persons
-  WHERE DAY(V2DATE) = day AND MONTH(V2DATE) = month AND YEAR(V2DATE) == year"""
+  actors_df = spark.read.parquet(config.OUTPUT_PATH+"/gkg_filtered_5themes.parquet").select("V1Persons", "V1Organizations")
 
-  res_persons = spark.sql(query_persons)
-
-  # counts = text_file.map(lambda line: line.split(";")) \
-  #   .flatMap(lambda line: [comb for comb in combinations(line, 2)]) \
-  #   .map(lambda word: (word, 1)) \
-  #   .reduceByKey(lambda a, b: a + b)
-
+  actors_df = actors_df.filter( \
+                      " OR ".join( \
+                        ['V1Persons like "%{}%"'.format(k) for k in top_50_persons] + \
+                        ['V1Organizations like "%{}%"'.format(k) for k in top_50_organizations]))
 
   path = config.OUTPUT_PATH+"actors_co-occurences.csv"
-  res_df.repartition(1).write.mode('overwrite').csv(path, header=True, sep=',')
+  actors_df.repartition(1).write.mode('overwrite').csv(path, header=True, sep=',')
 
-load_actor_occ(year, month, day)
+load_actor_occ()
