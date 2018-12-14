176ea3d0b703fbce272463bdb7b2a90641e1b20f
Mathieu <mathieu.ducroux@epfl.ch>
2018-12-14 17:22:30 +0100

    wip

--- src/local/helper.py
+++ src/local/helper.py
@@ -130,56 +130,67 @@ def wordcloud_organizations(DATA_PATH, nb_items, date=None, fig=None, pos=None):
     generate_wordcloud(organizations,nb_items, fig, pos)
 
 def plot_occ_graph(DATA_PATH, file, min_actor_rank, edge_size, node_weight_exp, spacing, figsize):
-    df = pd.read_csv(DATA_PATH + '/local_generated/' + file)
-    
-    # Read occurence data
-    per_occ = pd.read_csv(DATA_PATH + '/from_cluster/worldcloud_actors/persons_occurences.csv', nrows=min_actor_rank)
-    org_occ = pd.read_csv(DATA_PATH + '/from_cluster/worldcloud_actors/organizations_occurences.csv', nrows=min_actor_rank)
-    
-    # Create a dictionnary that maps an actor to its occurence
-    per_occ['Actor'] = per_occ['Actor'].apply(lambda x: x.replace(" ", "_"))
-    per_occ['Actor'] = per_occ['Actor'].apply(lambda x: x.replace("-", "_"))
-    per_occ_dict = per_occ.set_index('Actor').to_dict()['Count']
-    top_persons = list(per_occ_dict.keys())
-    org_occ['Actor'] = org_occ['Actor'].apply(lambda x: x.replace(" ", "_"))
-    org_occ['Actor'] = org_occ['Actor'].apply(lambda x: x.replace("-", "_"))
-    org_occ_dict = org_occ.set_index('Actor').to_dict()['Count']
-    top_organizations = list(org_occ_dict.keys())
-    actor_occ_dict = dict(per_occ_dict, **org_occ_dict)
-    
-    # Create the co-occurence matrix
-    docs = df.values.flatten().tolist()
-    count_model = CountVectorizer(ngram_range=(1,1)) # default unigram model
-    X = count_model.fit_transform(docs)
-    Xc = (X.T * X) # this is co-occurrence matrix in sparse csr format
-    Xc.setdiag(0)
-    
-    # Capitalize and remove underscore in actor names
-    nodes = count_model.get_feature_names()
-    nodes = [x.replace("_", " ") for x in nodes]
-    nodes = [' '.join([x.capitalize() for x in y.split()]) for y in nodes]
+	""" Generate a co-occurence graph of the top organizations and persons
 
-    # Create the weighted edge list
-    Knz = Xc.nonzero()
-    sparserows = Knz[0]
-    sparsecols = Knz[1]
-    edge_list = [x for x in list(zip(list(sparserows), list(sparsecols))) if x[0] <= x[1]]
-    weights = [Xc[x[0], x[1]]*0.001*edge_size for x in edge_list]
-    weighted_edge_list = [(x[0][0], x[0][1], x[1]) for x in list(zip(edge_list, weights))]
-    weighted_edge_list = [(nodes[x[0]], nodes[x[1]], x[2]) for x in weighted_edge_list]
+	Arguments:
+	file - file containing the co-occurence data
+	min_actor_rank - minimum rank to filter actor on
+	edge_size - size of edge of the graph
+	node_weight_exo - exponent of the node weight
+	spacing - degree of separation between the nodes of the graph
+	figsize - size of the matplotlib figure
+	"""
 
-    # Plot the graph
-    plt.subplots(figsize=(figsize,figsize))
-    G = nx.Graph()
-    G.add_weighted_edges_from(weighted_edge_list)
-    vertices = [x for x in G.nodes()]
-    vertices = [x.replace(" ", "_") for x in vertices]
-    vertices = [x.lower() for x in vertices]
-    node_col = ['#ff8d00' if x in top_organizations else '#00c900' for x in vertices]
-    node_sizes = [actor_occ_dict.get(x)**node_weight_exp for x in vertices]
-    pos = nx.spring_layout(G, k=spacing/(G.order()**0.5))
-    nx.draw(G, pos, with_labels=True, font_size = 12, font_weight = 'bold',
-            width=[G[u][v]['weight'] for u,v in G.edges()], node_size=node_sizes, font_color='k', node_color=node_col)
-    plt.axis('off')
-    plt.show()
+	df = pd.read_csv(DATA_PATH + '/local_generated/' + file)
+
+	# Read occurence data
+	per_occ = pd.read_csv(DATA_PATH + '/from_cluster/worldcloud_actors/persons_occurences.csv', nrows=min_actor_rank)
+	org_occ = pd.read_csv(DATA_PATH + '/from_cluster/worldcloud_actors/organizations_occurences.csv', nrows=min_actor_rank)
+
+	# Create a dictionnary that maps an actor to its occurence
+	per_occ['Actor'] = per_occ['Actor'].apply(lambda x: x.replace(" ", "_"))
+	per_occ['Actor'] = per_occ['Actor'].apply(lambda x: x.replace("-", "_"))
+	per_occ_dict = per_occ.set_index('Actor').to_dict()['Count']
+	top_persons = list(per_occ_dict.keys())
+	org_occ['Actor'] = org_occ['Actor'].apply(lambda x: x.replace(" ", "_"))
+	org_occ['Actor'] = org_occ['Actor'].apply(lambda x: x.replace("-", "_"))
+	org_occ_dict = org_occ.set_index('Actor').to_dict()['Count']
+	top_organizations = list(org_occ_dict.keys())
+	actor_occ_dict = dict(per_occ_dict, **org_occ_dict)
+
+	# Create the co-occurence matrix
+	docs = df.values.flatten().tolist()
+	count_model = CountVectorizer(ngram_range=(1,1)) # default unigram model
+	X = count_model.fit_transform(docs)
+	Xc = (X.T * X) # this is co-occurrence matrix in sparse csr format
+	Xc.setdiag(0)
+
+	# Capitalize and remove underscore in actor names
+	nodes = count_model.get_feature_names()
+	nodes = [x.replace("_", " ") for x in nodes]
+	nodes = [' '.join([x.capitalize() for x in y.split()]) for y in nodes]
+
+	# Create the weighted edge list
+	Knz = Xc.nonzero()
+	sparserows = Knz[0]
+	sparsecols = Knz[1]
+	edge_list = [x for x in list(zip(list(sparserows), list(sparsecols))) if x[0] <= x[1]]
+	weights = [Xc[x[0], x[1]]*0.001*edge_size for x in edge_list]
+	weighted_edge_list = [(x[0][0], x[0][1], x[1]) for x in list(zip(edge_list, weights))]
+	weighted_edge_list = [(nodes[x[0]], nodes[x[1]], x[2]) for x in weighted_edge_list]
+
+	# Plot the graph
+	plt.subplots(figsize=(figsize,figsize))
+	G = nx.Graph()
+	G.add_weighted_edges_from(weighted_edge_list)
+	vertices = [x for x in G.nodes()]
+	vertices = [x.replace(" ", "_") for x in vertices]
+	vertices = [x.lower() for x in vertices]
+	node_col = ['#ff8d00' if x in top_organizations else '#00c900' for x in vertices]
+	node_sizes = [actor_occ_dict.get(x)**node_weight_exp for x in vertices]
+	pos = nx.spring_layout(G, k=spacing/(G.order()**0.5))
+	nx.draw(G, pos, with_labels=True, font_size = 12, font_weight = 'bold',
+	        width=[G[u][v]['weight'] for u,v in G.edges()], node_size=node_sizes, font_color='k', node_color=node_col)
+	plt.axis('off')
+	plt.show()
 
